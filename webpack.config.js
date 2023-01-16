87eebdba0e6edbafa4b5ce5834b5955851be676a
artmeister <artiom.mamadaliev@yandex.by>
2023-01-17 00:59:09 +0300

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
 const path = require('path');
 
-// css extraction and minification
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-// clean out build dir in-between builds
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = [
   {
     entry: {
       'main': [
-        './js/src/main.js',
-        './css/src/main.scss'
+        './js/main.js',
+        './css/main.scss'
       ]
     },
     output: {
@@ -21,18 +19,15 @@ module.exports = [
     },
     module: {
       rules: [
-        // js babelization
         {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           loader: 'babel-loader'
         },
-        // sass compilation
         {
           test: /\.(sass|scss)$/,
           use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
         },
-        // loader for webfonts (only required if loading custom fonts)
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
           type: 'asset/resource',
@@ -40,7 +35,6 @@ module.exports = [
             filename: './css/build/font/[name][ext]',
           }
         },
-        // loader for images and icons (only required if css references image files)
         {
           test: /\.(png|jpg|gif)$/,
           type: 'asset/resource',
@@ -59,24 +53,19 @@ module.exports = [
       ]
     },
     plugins: [
-      // clear out build directories on each build
       new CleanWebpackPlugin({
         cleanOnceBeforeBuildPatterns: [
           './js/build/*',
           './css/build/*'
         ]
       }),
-      // css extraction into dedicated file
       new MiniCssExtractPlugin({
         filename: './css/build/main.min.[fullhash].css'
       }),
     ],
     optimization: {
-      // minification - only performed when mode = production
       minimizer: [
-        // js minification - special syntax enabling webpack 5 default terser-webpack-plugin 
         `...`,
-        // css minification
         new CssMinimizerPlugin(),
       ]
     },
